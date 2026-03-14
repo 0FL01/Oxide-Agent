7af49bf2e8d966ba7e703e7ef8900492b118ba03
Andrey Bash <andrej89536995626@gmail.com>
2026-03-14 19:15:26 +0300

    fix

---
+++ test_forum_method.rs
@@ -0,0 +1,13 @@
+use teloxide::prelude::*;
+
+fn main() {
+    // Test if forum topic methods are available
+    // This should fail to compile if they don't exist
+    // create_forum_topic(chat_id, name)
+    // edit_forum_topic(chat_id, thread_id, ...)
+    // close_forum_topic(chat_id, thread_id)
+    // reopen_forum_topic(chat_id, thread_id)
+    // delete_forum_topic(chat_id, thread_id)
+    // get_forum_topic(chat_id, thread_id)
+    let _ = "compile_check";
+}
